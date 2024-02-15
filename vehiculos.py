a0d532ae7048ce7d20ec5c09d25354e6557a37c0
annakfigueroa <156444494+annakfigueroa@users.noreply.github.com>
2024-02-15 07:57:08 -0400

    vehiculos.py

--- vehiculos.py
+++ vehiculos.py
@@ -1,30 +1,30 @@
-class Vehicles:
+class Vehicle:
     registro = 'Registro de Vehículos'
     def __init__(self,type,brand,color,place):
         self.type=type
         self.brand=brand
         self.color=color
         self.place=place
-class Car(Vehicles):
+class Car(Vehicle):
     def __init__(self,type,brand,color,place,year,plate):
         super().__init__(type,brand,color,place)
         self.year=year
         self.plate=plate
-    def carrito(self): 
+    def show_attr1(self): 
         return "Tipo de Vehículo:{}, Marca: {}, Color: {}, Lugar de registro: {}, Año de registro: {}, Placa: {}".format(self.type, self.brand, self.color, self.place,self.year,self.plate) 
-class Boat(Vehicles):
+class Boat(Vehicle):
     def __init__(self,type,brand,color,place,nameb,typeb):
         super().__init__(type,brand,color,place)
         self.nameb=nameb
         self.typeb=typeb
-    def bote(self):
+    def show_attr2(self):
         return "Tipo de Vehículo:{}, Marca: {}, Color: {}, Lugar de registro: {}, Nombre del barco{},Tipo de barco: {}".format(self.type, self.brand, self.color, self.place,self.nameb,self.typeb)
-class Plane(Vehicles):
+class Plane(Vehicle):
     def __init__(self,type,brand,color,place,vlicense,pilotlicense):
         super().__init__(type,brand,color,place)
         self.vlicense=vlicense
         self.pilotlicense=pilotlicense
-    def volar(self):
+    def show_attr3(self):
         return "Tipo de Vehículo:{}, Marca: {}, Color: {}, Lugar de registro: {}, Licencia: {}, Licencia piloto: {}".format(self.type, self.brand, self.color, self.place,self.vlicense,self.pilotlicense)
 
 corolla=Car("Carro","Toyota","Plateado","Merida",2009,"A9876YH")
@@ -34,17 +34,15 @@ perlanegra=Boat("Bote","Pirate","Marron","Caribe","Perla Negra","Galeón")
 yaterandom=Boat("Bote","Oceanco","Blanco","Trinidad y Tobago","Octopus","Yate")
 
 avion1=Plane("Avión","Airbus","Azul","Caracas","YT123",389898)
-avion2=Plane("Avión","Boeing","Verde","Táchira","AB897",908394-1)
-
+avion2=Plane("Avión","Boeing","Verde","Táchira","AB897",908391)
 print("--> Carros")
-print(corolla.carrito())
-print(supra.carrito())
+print(corolla.show_attr1())
+print(supra.show_attr1())
 print()
 print("-->Botes")
-print(perlanegra.bote())
-print(yaterandom.bote())
+print(perlanegra.show_attr2())
+print(yaterandom.show_attr2())
 print()
 print("-->Aviones")
-print(avion1.volar())
-print(avion2.volar())
-
+print(avion1.show_attr3())
+print(avion2.show_attr3())
